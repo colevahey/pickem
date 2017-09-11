f0b7ea9d69f8e67bbd5d27af8bfe80bacd55c445
Cole Vahey <rcvahey01@gmail.com>
2017-09-11 17:26:13 -0400

    Just testing some indexing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import urllib.request
+import json
 
 rawhtml = urllib.request.urlopen("https://cfb-scoreboard-api.herokuapp.com/v1/date/20170916")
 
 data = rawhtml.read().decode("utf-8")
-print(data)
+
+jsondata = json.loads(data)
+print(jsondata)
+
+for game in range(0, len(jsondata["games"])):
+    print(jsondata["games"][game]["id"])
