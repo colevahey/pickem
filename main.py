2df0adfd190ae615b7d9f15c616e193cefcf9c3b
Cole Vahey <rcvahey01@gmail.com>
2017-09-11 17:16:43 -0400

    Starting over with a great API I just found

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-import os
-import re
+import urllib.request
 
-#Curl the html from USAToday Odds Website as a string
-rawhtml = os.popen("curl https://www.usatoday.com/sports/ncaaf/odds/").read()
+rawhtml = urllib.request.urlopen("https://cfb-scoreboard-api.herokuapp.com/v1/date/20170916")
 
-#Find the start and end of the table with all the teams in it
-starttable = re.search("<tbody", rawhtml).span()[0]
-endtable = re.search("</tbody", rawhtml).span()[1]
-
-print(starttable)
-print(endtable)
-
-print("THE TABLE LOOKS LIKE THIS:\n\n\n")
-
-#Slice the html at the table start and end points
-table = rawhtml[starttable:endtable]
-print(table)
-
-#Find "Penn State" in the table
-pennstate = re.search("Penn State", table)
-print(pennstate)
-
-#Print the table where Penn State was found
-print(table[pennstate.span()[0]:pennstate.span()[1]])
+data = rawhtml.read().decode("utf-8")
+print(data)
