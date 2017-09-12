d4c9511d453a816b220b15b303257847c6fa39de
Cole Vahey <rcvahey01@gmail.com>
2017-09-11 22:16:35 -0400

    Nicer GUI

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import json
 
 rawhtml = urllib.request.urlopen("https://cfb-scoreboard-api.herokuapp.com/v1/date/20170916")
 
-data = rawhtml.read().decode("utf-8")
+data = json.loads(rawhtml.read().decode("utf-8"))
 
-jsondata = json.loads(data)
-print(jsondata)
+#For each game in the database for that week
+for game in range(0, len(data["games"])):
 
-for game in range(0, len(jsondata["games"])):
-    print(jsondata["games"][game]["id"])
+    #If the game has a spread
+    if data["games"][game]["odds"]["spread"] != "N/A":
+
+        #Print the teams playing in the game
+        print(data["games"][game]["homeTeam"]["displayName"] + " vs. " + data["games"][game]["awayTeam"]["displayName"])
+
+        #Print the spread of the game
+        print(data["games"][game]["odds"]["spread"])
+        print()
