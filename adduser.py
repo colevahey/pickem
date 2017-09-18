590b1d425c7d105186ebd0aca7711f6ee9c04f28
Cole Vahey <rcvahey01@gmail.com>
2017-09-18 19:51:12 -0400

    Hashes working plus a new selection process with a dictionary of the game id and projected winning team

--- adduser.py
+++ adduser.py
@@ -1,34 +1,38 @@
 import hashlib
 import json
 
-uname = input("New Username?\n>> ")
-password = input("New Password?\n>> \033[8m")
-print("\033[0m")
+def main():
+    uname = input("New Username?\n>> ")
+    password = input("New Password?\n>> \033[8m")
+    print("\033[0m")
 
-users = open('users.json', 'r')
-userdata = json.loads(users.read())
+    users = open('users.json', 'r')
+    userdata = json.loads(users.read())
 
-hash_object = hashlib.md5(password.encode())
-hashpassword = hash_object.hexdigest()
+    hash_object = hashlib.md5(password.encode())
+    hashpassword = hash_object.hexdigest()
 
-try:
-    userdata[uname]
-    print("User " + uname + " already exists.")
-    updatepword = input("Update password (Y/N)? ")
-    if updatepword.upper() == "Y":
+    try:
+        userdata[uname]
+        print("User " + uname + " already exists.")
+        updatepword = input("Update password (Y/N)? ")
+        if updatepword.upper() == "Y":
+            pass
+        else:
+            print("Aborted")
+            exit()
+    except KeyError:
         pass
-    else:
-        print("Aborted")
-        exit()
-except KeyError:
-    pass
 
-userdata[uname] = hashpassword
+    userdata[uname] = {"password": hashpassword, "wins": 0, "losses":0}
 
-userdatafile = open('users.json', 'w')
+    userdatafile = open('users.json', 'w')
 
-userdatafile.write(
-    json.dumps(userdata)
-)
+    userdatafile.write(
+        json.dumps(userdata)
+    )
 
-print("User " + uname + " successfully added")
+    print("User " + uname + " successfully added")
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import urllib.request
 import json
 from time import sleep
 import hashlib
+import adduser
+
+rawhtml = urllib.request.urlopen("https://cfb-scoreboard-api.herokuapp.com/v1/date/20170923")
+
+data = json.loads(rawhtml.read().decode("utf-8"))
 
 def login():
     uname = input("Username?\n>> ")
@@ -15,9 +20,12 @@ def login():
     hashpassword = hash_object.hexdigest()
 
     try:
-        if userdata[uname] == hashpassword:
-            print("Welcome, " + uname)
-            main()
+        if userdata[uname]["password"] == hashpassword:
+            print("Welcome back " + uname)
+            print("Your current record is " + str(userdata[uname]["wins"]) + "-" + str(userdata[uname]["losses"]))
+            print()
+            sleep(3)
+            selections(uname)
         else:
             print("That password is incorrect")
             input("Try again? [Press enter to continue]")
@@ -27,16 +35,14 @@ def login():
         print("That user does not exist")
         create = input("Try again [Press enter] or create new user [Press c enter]?\n>> ")
         if create.lower() == "c":
-            print("DO THE CREATE THING")
+            print()
+            print("NEW USER:\n")
+            adduser.main()
         else:
             print()
             login()
 
 def main():
-    rawhtml = urllib.request.urlopen("https://cfb-scoreboard-api.herokuapp.com/v1/date/20170923")
-
-    data = json.loads(rawhtml.read().decode("utf-8"))
-
     #For each game in the database for that week
     for game in data["games"]:
 
@@ -44,10 +50,32 @@ def main():
         if game["odds"]["spread"] != "N/A":
 
             #Print the teams playing in the game
-            print(game["homeTeam"]["displayName"] + " vs. " + game["awayTeam"]["displayName"])
+            print(game["awayTeam"]["displayName"] + " at " + game["homeTeam"]["displayName"])
 
             #Print the spread of the game
             print(game["odds"]["spread"])
             print()
+            sleep(1)
+
+def selections(user):
+
+    userselections = {}
+
+    for game in data["games"]:
+        if game["odds"]["spread"] != "N/A":
+            print("\n" + game["awayTeam"]["displayName"] + " at " + game["homeTeam"]["displayName"])
+            print(game["odds"]["spread"])
+            selection = input("Who do you select? (A/H)\n>> ").title()
+            if selection == "A":
+                print("You selected " + game["awayTeam"]["displayName"])
+                userselections.update({game["id"]:game["awayTeam"]["displayName"]})
+            elif selection == "H":
+                print("You selected " + game["homeTeam"]["displayName"])
+                userselections.update({game["id"]:game["homeTeam"]["displayName"]})
+            else:
+                print("That pick is invalid. You have to start over now.")
+                input("Press enter to continue")
+
+    print(userselections)
 
 login()
