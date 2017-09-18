3fa1fe1dfda0c02193966e502673b2c7d6a5c5a7
Cole Vahey <rcvahey01@gmail.com>
2017-09-17 23:02:37 -0400

    Hashing of passwords working with a check to users file

---
+++ adduser.py
@@ -0,0 +1,34 @@
+import hashlib
+import json
+
+uname = input("New Username?\n>> ")
+password = input("New Password?\n>> \033[8m")
+print("\033[0m")
+
+users = open('users.json', 'r')
+userdata = json.loads(users.read())
+
+hash_object = hashlib.md5(password.encode())
+hashpassword = hash_object.hexdigest()
+
+try:
+    userdata[uname]
+    print("User " + uname + " already exists.")
+    updatepword = input("Update password (Y/N)? ")
+    if updatepword.upper() == "Y":
+        pass
+    else:
+        print("Aborted")
+        exit()
+except KeyError:
+    pass
+
+userdata[uname] = hashpassword
+
+userdatafile = open('users.json', 'w')
+
+userdatafile.write(
+    json.dumps(userdata)
+)
+
+print("User " + uname + " successfully added")
--- main.py
+++ main.py
@@ -3,24 +3,7 @@ import json
 from time import sleep
 import hashlib
 
-def main():
-    rawhtml = urllib.request.urlopen("https://cfb-scoreboard-api.herokuapp.com/v1/date/20170916")
-
-    data = json.loads(rawhtml.read().decode("utf-8"))
-
-    #For each game in the database for that week
-    for game in range(0, len(data["games"])):
-
-        #If the game has a spread
-        if data["games"][game]["odds"]["spread"] != "N/A":
-
-            #Print the teams playing in the game
-            print(data["games"][game]["homeTeam"]["displayName"] + " vs. " + data["games"][game]["awayTeam"]["displayName"])
-
-            #Print the spread of the game
-            print(data["games"][game]["odds"]["spread"])
-            print()
-
+def login():
     uname = input("Username?\n>> ")
     password = input("Password?\n>> \033[8m")
     print("\033[0m")
@@ -28,16 +11,43 @@ def main():
     users = open('users.json', 'r')
     userdata = json.loads(users.read())
 
+    hash_object = hashlib.md5(password.encode())
+    hashpassword = hash_object.hexdigest()
+
     try:
-        if userdata[uname] == password:
+        if userdata[uname] == hashpassword:
             print("Welcome, " + uname)
+            main()
         else:
             print("That password is incorrect")
             input("Try again? [Press enter to continue]")
-            main()
+            print()
+            login()
     except KeyError:
         print("That user does not exist")
-        input("Try again? [Press enter to continue]")
-        main()
+        create = input("Try again [Press enter] or create new user [Press c enter]?\n>> ")
+        if create.lower() == "c":
+            print("DO THE CREATE THING")
+        else:
+            print()
+            login()
 
-main()
+def main():
+    rawhtml = urllib.request.urlopen("https://cfb-scoreboard-api.herokuapp.com/v1/date/20170923")
+
+    data = json.loads(rawhtml.read().decode("utf-8"))
+
+    #For each game in the database for that week
+    for game in data["games"]:
+
+        #If the game has a spread
+        if game["odds"]["spread"] != "N/A":
+
+            #Print the teams playing in the game
+            print(game["homeTeam"]["displayName"] + " vs. " + game["awayTeam"]["displayName"])
+
+            #Print the spread of the game
+            print(game["odds"]["spread"])
+            print()
+
+login()
