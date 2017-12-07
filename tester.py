a4b92218c6cd7b695b07eb2667303e5f5ba43eaf
colevahey <rcvahey01@gmail.com>
2017-12-06 23:58:15 -0500

    Trying to do away with the Saturdays file... Making the whichweek functionality more efficient.

---
+++ tester.py
@@ -0,0 +1,4 @@
+import calendar as c
+
+c.setfirstweekday(c.SATURDAY)
+print(c.monthcalendar(2017,11))
--- whichweek.py
+++ whichweek.py
@@ -1,5 +1,18 @@
 from datetime import date, timedelta
+import calendar
 import json
+
+def saturdaysfromcalendar():
+    calendar.setfirstweekday(calendar.SATURDAY)
+
+    saturdaysfromcal = []
+
+    for week in calendar.monthcalendar(date.today().year,date.today().month):
+        if week[0] != 0:
+            saturdaysfromcal.append(week[0])
+
+    print(saturdaysfromcal)
+
 
 def getweek():
 
@@ -9,21 +22,18 @@ def getweek():
     today = date.today()
 
     if today.weekday() == 5:
-
         nextsaturday = today + timedelta(days=7)
         lastsaturday = today
         nextsaturday = str(nextsaturday).replace("-","")
         lastsaturday = str(lastsaturday).replace("-","")
 
     elif today.weekday() == 6:
-
         nextsaturday = today + timedelta(days=6)
         lastsaturday = today - timedelta(days=1)
         nextsaturday = str(nextsaturday).replace("-","")
         lastsaturday = str(lastsaturday).replace("-","")
 
     else:
-
         nextsaturday = today + timedelta(days=abs(5-today.weekday()))
         lastsaturday = today - timedelta(days=abs(-2-today.weekday()))
         nextsaturday = str(nextsaturday).replace("-","")
@@ -33,5 +43,8 @@ def getweek():
     return {"next":[inversesaturdays[nextsaturday],nextsaturday],"last":[inversesaturdays[lastsaturday],lastsaturday]}
 
 if __name__ == '__main__':
+
+    saturdaysfromcalendar()
+
     print("Next Saturday is week " + getweek()["next"][0] + " on " + getweek()["next"][1])
     print("Last Saturday was week " + getweek()["last"][0] + " on " + getweek()["last"][1])
