952d592957de562075998b415de67f3de49febcf
colevahey <rcvahey01@gmail.com>
2018-01-09 22:57:46 -0500

    Going to try and do this as a website... Would be very cool if I could

--- py/whichweek.py
+++ py/whichweek.py
@@ -1,36 +1,6 @@
 from datetime import date, timedelta
-import calendar
 import json
 
-def saturdaysfromcalendar():
-    today = date.today().day
-    calendar.setfirstweekday(calendar.SATURDAY)
-    saturdaysfromcal = []
-    lastsat = 0
-    nextsat = 0
-    for week in calendar.monthcalendar(date.today().year,date.today().month):
-        if week[0] != 0:
-            saturdaysfromcal.append(week[0])
-    for day in saturdaysfromcal:
-        if day != 0:
-            if day + 7 < today or day - 7 > today:
-                pass
-            else:
-                if today - day >= 0:
-                    lastsat = day
-                else:
-                    nextsat = day
-    if lastsat == 0:
-        if date.today().month != 1:
-            lastsat = calendar.monthcalendar(date.today().year, date.today().month-1)[-1][0]
-        else:
-            lastsat = calendar.monthcalendar(date.today().year-1,12)[-1][0]
-    if nextsat == 0:
-        if date.today().month != 12:
-            nextsat = calendar.monthcalendar(date.today().year, date.today().month+1)[1][0]
-        else:
-            nextsat = calendar.monthcalendar(date.today().year+1, 1)[1][0]
-            
 def getweek():
 
     saturdays = json.load(open('saturdays.json','r'))
@@ -55,12 +25,10 @@ def getweek():
         lastsaturday = today - timedelta(days=abs(-2-today.weekday()))
         nextsaturday = str(nextsaturday).replace("-","")
         lastsaturday = str(lastsaturday).replace("-","")
-    
+
     return {"next":[inversesaturdays[nextsaturday],nextsaturday],"last":[inversesaturdays[lastsaturday],lastsaturday]}
 
 if __name__ == '__main__':
-
-    saturdaysfromcalendar()
 
     print("Next Saturday is week " + getweek()["next"][0] + " on " + getweek()["next"][1])
     print("Last Saturday was week " + getweek()["last"][0] + " on " + getweek()["last"][1])
