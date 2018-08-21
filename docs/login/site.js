68c7a081f730b6099459fe7e76fbf876bfe658c0
colevahey <rcvahey01@gmail.com>
2018-08-21 18:00:30 -0400

    Removed the login page because I will not need it in the future

--- docs/login/site.js
+++
@@ -1,5 +0,0 @@
-function login(){
-  let username = document.getElementsByClassName("user")[0].value
-  let password = document.getElementsByClassName("pass")[0].value
-  console.log("Username:",username,"Password:",password)
-}
