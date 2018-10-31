b30316d169fc6ef69199379f770a4f3d5d787652
colevahey <rcvahey01@gmail.com>
2018-10-31 15:06:43 -0400

    Much refacturing wow

--- site.js
+++ site.js
@@ -47,6 +47,45 @@ const fetchData = async date => {
   setGames(data)
 }
 
+const teamSet = (teamData, teamNode, gameNum) => {
+  teamNode.className = teamData.homeAway
+  teamNode.selected = false
+
+  teamNode.onclick = _ => select(teamData.homeAway, gameNum)
+    
+  teamNode.onmouseover = e => {
+    if (e.target.selected == false) {
+      e.target.style = "background-color:#707070"
+    }
+    e.target.style.cursor = "pointer"
+  }
+  teamNode.onmouseout = e => {
+    if (e.target.selected == false){
+      e.target.style = "background-color:grey"
+    }
+    e.target.style.cursor = "default"
+  }
+
+  if (teamData.curatedRank.current < 99 && teamData.curatedRank.current != 0) {
+    teamNode.innerHTML = "#" + teamData.curatedRank.current + " "
+  } else {
+    teamNode.innerHTML = ""
+  }
+
+  teamNode.innerHTML += teamData.team.displayName
+  let teamClubhouse = document.createElement("a")
+  teamClubhouse.href = teamData.team.links[0].href
+  teamClubhouse.target = "_blank"
+  let teamImage = document.createElement("img")
+  teamImage.src = teamData.team.logo
+  teamImage.height = "100"
+  teamImage.style = "float:left;"
+  teamClubhouse.appendChild(teamImage)
+  teamNode.appendChild(teamClubhouse)
+
+  return teamNode
+}
+
 const setGames = gameData => {
 
   for (let i=0;i<gameData.events.length;i++){
@@ -61,61 +100,6 @@ const setGames = gameData => {
     let homeTeam = document.createElement("td")
     let spread = document.createElement("td")
 
-    homeTeam.className = "home"
-    homeTeam.selected = false
-    //homeTeam.onclick = function(){select(["home",i])}
-    homeTeam.onclick = _ => select(["home",i])
-    
-    homeTeam.onmouseover = e => {
-      if (e.target.selected == false) {
-        e.target.style = "background-color:#707070"
-      }
-      e.target.style.cursor = "pointer"
-    }
-    homeTeam.onmouseout = e => {
-      if (e.target.selected == false){
-        e.target.style = "background-color:grey"
-      }
-      e.target.style.cursor = "default"
-    }
-
-    if (homeData.curatedRank.current < 99 && homeData.curatedRank.current != 0) {
-      homeTeam.innerHTML = "#" + homeData.curatedRank.current + " "
-    } else {
-      homeTeam.innerHTML = ""
-    }
-    homeTeam.innerHTML += homeData.team.displayName
-    let homeClubhouse = document.createElement("a")
-    homeClubhouse.href = homeData.team.links[0].href
-    homeClubhouse.target = "_blank"
-    let homeImage = document.createElement("img")
-    homeImage.src = homeData.team.logo
-    homeImage.height = "100"
-    homeImage.style = "float:left;"
-    homeClubhouse.appendChild(homeImage)
-    homeTeam.appendChild(homeClubhouse)
-
-    awayTeam.className = "away"
-    awayTeam.selected = false
-    awayTeam.onclick = function(){select(["away",i])}
-    awayTeam.onmouseover = function(){if (awayTeam.selected == false){awayTeam.style = "background-color:#707070"}; this.style.cursor = "pointer"}
-    awayTeam.onmouseout = function(){if (awayTeam.selected == false){awayTeam.style = "background-color:grey"}; this.style.cursor = "default"}
-    if (awayData.curatedRank.current < 99 && awayData.curatedRank.current != 0) {
-      awayTeam.innerHTML = "#" + awayData.curatedRank.current + " "
-    } else {
-      awayTeam.innerHTML = ""
-    }
-    awayTeam.innerHTML += awayData.team.displayName
-    let awayClubhouse = document.createElement("a")
-    awayClubhouse.href = awayData.team.links[0].href
-    awayClubhouse.target = "_blank"
-    let awayImage = document.createElement("img")
-    awayImage.src = awayData.team.logo
-    awayImage.height = "100"
-    awayImage.style = "float:right;"
-    awayClubhouse.appendChild(awayImage)
-    awayTeam.appendChild(awayClubhouse)
-
     spread.className = "versus"
     if (game.status.type.completed != true) {
       if (game.odds) {
@@ -127,37 +111,26 @@ const setGames = gameData => {
       spread.innerHTML = homeData.score + "  -  " + awayData.score
     }
 
-    newGame.appendChild(homeTeam)
+    newGame.appendChild(teamSet(homeData, homeTeam, i))
     newGame.appendChild(spread)
-    newGame.appendChild(awayTeam)
+    newGame.appendChild(teamSet(awayData, awayTeam, i))
     document.querySelector("#gamestable").appendChild(newGame)
 
   }
 }
 
-const select = teamnum => {
-  homeTeams = document.getElementsByClassName("home")
-  awayTeams = document.getElementsByClassName("away")
-  if (teamnum[0] == "home") {
-    if (homeTeams[teamnum[1]].selected == false) {
-      homeTeams[teamnum[1]].style = "background-color:#ff2d2d;"
-      homeTeams[teamnum[1]].selected = true
-      awayTeams[teamnum[1]].style = "background-color:grey;"
-      awayTeams[teamnum[1]].selected = false
-    } else {
-      homeTeams[teamnum[1]].style = "background-color:grey;"
-      homeTeams[teamnum[1]].selected = false
-    }
+const select = (homeAway, teamNum) => {
+  selectedTeam = document.getElementsByClassName(homeAway)[teamNum]
+  partnerTeam = document.getElementsByClassName("homeaway".replace(homeAway,""))[teamNum]
+  
+  if (selectedTeam.selected == false) {
+    selectedTeam.style = "background-color:#ff2d2d;"
+    selectedTeam.selected = true
+    partnerTeam.style = "background-color:grey;"
+    partnerTeam.selected = false
   } else {
-    if (awayTeams[teamnum[1]].selected == false) {
-      awayTeams[teamnum[1]].style = "background-color:#ff2d2d;"
-      awayTeams[teamnum[1]].selected = true
-      homeTeams[teamnum[1]].style = "background-color:grey;"
-      homeTeams[teamnum[1]].selected = false
-    } else {
-      awayTeams[teamnum[1]].style = "background-color:grey;"
-      awayTeams[teamnum[1]].selected = false
-    }
+    selectedTeam.style = "background-color:grey;"
+    selectedTeam.selected = false
   }
 }
 
@@ -170,26 +143,24 @@ const finalCheck = gameDate => {
   for (let i=0; i<games.length; i++) {
     selections[gameDate]["Game"+i] = {}
     let game = games[i]
-    switch (game.cells[0].selected){
-      case true:
-        console.log("Game",i+1,"home selected")
+    if (game.cells[0].selected) {
+      console.log("Game",i+1,"home selected")
+      totalSelected += 1
+      selections[gameDate]["Game" + i]["team"] = "HOME"
+      selections[gameDate]["Game" + i]["spread"] = game.querySelector(".versus").innerHTML
+      break
+    } else {
+      if (game.cells[2].selected) {
+        console.log("Game",i+1,"away selected")
         totalSelected += 1
-        selections[gameDate]["Game" + i]["team"] = "HOME"
+        selections[gameDate]["Game" + i]["team"] = "AWAY"
         selections[gameDate]["Game" + i]["spread"] = game.querySelector(".versus").innerHTML
         break
-      case false:
-        switch (game.cells[2].selected){
-          case true:
-            console.log("Game",i+1,"away selected")
-            totalSelected += 1
-            selections[gameDate]["Game" + i]["team"] = "AWAY"
-            selections[gameDate]["Game" + i]["spread"] = game.querySelector(".versus").innerHTML
-            break
-          case false:
-            console.log("NO TEAM SELECTED GAME",i+1)
-            notSelected += i+1+" "
-            break
-        }
+      } else {
+        console.log("NO TEAM SELECTED GAME",i+1)
+        notSelected += i+1+" "
+        break
+      }
     }
   }
   console.log(selections)
